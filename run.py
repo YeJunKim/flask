6e3e047a2ff0dd0478e82a3f9df18205a051e446
YeJunKim <sillon2808@gmail.com>
2018-05-11 13:40:50 +0900

    modify css

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+@app.route('/test')
+def test():
+    return render_template('a.html')
+
 @app.route('/')
 def main():
     return render_template('index.html')
